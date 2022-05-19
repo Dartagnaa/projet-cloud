13370bb3fa693dc7e3c7da0b6acb6d6de7a2a6b7
Eradid <corentin.montiege@gmail.com>
2022-05-19 15:19:38 +0200

    bouton GET

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+
+class App extends React.Component {
+
+  GetTest() {
+    // Simple GET request using fetch
+    fetch('https://api.npms.io/v2/search?q=react')
+        .then(response => response.json());
+  }
+
+  render() {
+    return  (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -17,9 +29,11 @@ function App() {
         >
           Learn React
         </a>
+        <Button type="primary" onClick={this.GetTest}>GET</Button>
       </header>
     </div>
-  );
+    );    
+  }
 }
 
 export default App;
